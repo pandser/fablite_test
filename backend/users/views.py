4051c95b26d2019f9cbd44e997da75172c2a2a0b
Андрей Полушин <pandser@yandex.ru>
2024-07-16 09:01:10 +0300

    create urls

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
+from django.contrib.auth.views import (
+    PasswordChangeView, PasswordResetView, PasswordResetConfirmView
+)
 
-# Create your views here.
+from users.forms import CreationForm
+
+
+class SignUp(CreateView):
+    form_class = CreationForm
+    success_url = reverse_lazy('users:login')
+    template_name = 'users/signup.html'
+
+
+class PasswordChange(PasswordChangeView):
+    success_url = reverse_lazy('users:password_change_done')
+    template_name = 'users/password_change_form.html'
+
+
+class PasswordReset(PasswordResetView):
+    success_url = reverse_lazy('users:password_reset_done')
+    template_name = 'users/password_reset_form.html'
+
+
+class PasswordResetConfirm(PasswordResetConfirmView):
+    success_url = reverse_lazy('users:password_reset_complete')
+    template_name = 'users/password_reset_confirm.html'
